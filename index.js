e0d124780254cb1b6c3a5da3e4fa4644773f6c0c
manavmalavia18 <114517641+manavmalavia18@users.noreply.github.com>
2023-11-24 12:34:38 -0500

    lambda1

---
+++ index.js
@@ -0,0 +1,87 @@
+const formData = require('form-data');
+const Mailgun = require('mailgun.js');
+const mailgun = new Mailgun(formData);
+const { Storage } = require('@google-cloud/storage');
+const axios = require('axios');
+
+
+const mg = mailgun.client({
+    username: 'api',
+    key: process.env.MAILGUN_API_KEY, 
+});
+
+
+const sendMail = async (sender_email, receiver_email, email_subject, email_body) => {
+    const data = {
+        from: sender_email,
+        to: receiver_email,
+        subject: email_subject,
+        text: email_body
+    };
+
+    try {
+        const body = await mg.messages.create('manavmalavia.me', data); 
+        console.log(body);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+const gcpServiceKey = JSON.parse(process.env.GCP_SERVICE_ACCOUNT_KEY);
+const storage = new Storage({
+    credentials: gcpServiceKey
+});
+const bucketName = 'webappbucket_manav'; // Replace with your GCP bucket name
+
+const downloadAndUploadToGCS = async (url, gcsFileName) => {
+    try {
+        const response = await axios({
+            method: 'get',
+            url: url,
+            responseType: 'stream'
+        });
+
+        const file = storage.bucket(bucketName).file(gcsFileName).createWriteStream({
+            metadata: {
+                contentType: 'application/zip' // Adjust content type if necessary
+            }
+        });
+
+        return new Promise((resolve, reject) => {
+            response.data.pipe(file)
+                .on('finish', () => {
+                    resolve(`File uploaded to ${gcsFileName} in bucket ${bucketName}`);
+                })
+                .on('error', (error) => {
+                    reject(`Error uploading to ${gcsFileName}: ${error}`);
+                });
+        });
+    } catch (error) {
+        console.error('Error downloading file:', error);
+        throw error;
+    }
+};
+exports.handler = async (event) => {
+    console.log("Received SNS event:", JSON.stringify(event, null, 2));
+
+    const record = event.Records[0];
+    const snsMessage = JSON.parse(record.Sns.Message);
+    const receiver_email = snsMessage.userEmail;  
+    const submissionUrl = snsMessage.submission_url; 
+
+    let sender_email = 'mailgun@manavmalavia.me'; 
+    let email_subject = 'Mailgun Test';
+    let email_body = 'Data received from SNS';
+
+
+
+    await sendMail(sender_email, receiver_email, email_subject, email_body);
+
+    try {
+        const gcsFileName = 'webappbucket_manav/webapp'; 
+        const message = await downloadAndUploadToGCS(submissionUrl, gcsFileName);
+        console.log(message);
+    } catch (error) {
+        console.error('Error handling file:', error);
+    }
+};
